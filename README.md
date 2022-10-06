0d37ae115aeb7579a525387ce2387751d41fc036
Amanda Lange <litagemini@gmail.com>
2022-10-06 14:10:46 -0400

    basic code to test deployment

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 This is an Alexa skill designed to interface (eventually) with the Final Fantasy XIV API to get server data etc. - https://xivapi.com/
 
 Still learning how to properly do this.
+
+Some of this code is borrowed from the alexa-samples github as a starting point: https://github.com/alexa-samples/skill-sample-python-fact. As such, included the license file here for that work.
--- lambda/py/lambda_function.py
+++ lambda/py/lambda_function.py
@@ -1,5 +1,8 @@
 import logging
 import ask_sdk_core.utils as ask_utils
+import json
+import prompts
+import random
 
 from ask_sdk_core.skill_builder import SkillBuilder
 from ask_sdk_core.dispatch_components import AbstractRequestHandler
@@ -11,7 +14,6 @@ from ask_sdk_core.handler_input import HandlerInput
 from ask_sdk_model import Response
 
 import utils
-import weatherClient
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -34,19 +36,26 @@ class GetDataApiHandler(AbstractRequestHandler):
 
         return response
 
-
-class SessionEndedRequestHandler(AbstractRequestHandler):
-    """Handler for Session End."""
+class GetServerNameHandler(AbstractRequestHandler):
+    '''let's get the name of a server from the list eventually API'''
 
     def can_handle(self, handler_input):
         # type: (HandlerInput) -> bool
-        return ask_utils.is_request_type("SessionEndedRequest")(handler_input)
+        return (is_request_type("LaunchRequest")(handler_input) or
+                is_intent_name("GetNewServerIntent")(handler_input))
 
     def handle(self, handler_input):
         # type: (HandlerInput) -> Response
+        logger.info("In GetServerNameHandler")
 
-        # Any cleanup logic goes here.
+        # get localization data
+        data = handler_input.attributes_manager.request_attributes["_"]
 
+        random_fact = random.choice(data[prompts.FACTS])
+        speech = data[prompts.GET_FACT_MESSAGE].format(random_fact)
+
+        handler_input.response_builder.speak(speech).set_card(
+            SimpleCard(data[prompts.SKILL_NAME], random_fact))
         return handler_input.response_builder.response
 
 
@@ -73,6 +82,84 @@ class IntentReflectorHandler(AbstractRequestHandler):
             .response
         )
 
+class HelpIntentHandler(AbstractRequestHandler):
+    """Handler for Help Intent."""
+
+    def can_handle(self, handler_input):
+        # type: (HandlerInput) -> bool
+        return is_intent_name("AMAZON.HelpIntent")(handler_input)
+
+    def handle(self, handler_input):
+        # type: (HandlerInput) -> Response
+        logger.info("In HelpIntentHandler")
+
+        # get localization data
+        data = handler_input.attributes_manager.request_attributes["_"]
+
+        speech = data[prompts.HELP_MESSAGE]
+        reprompt = data[prompts.HELP_REPROMPT]
+        handler_input.response_builder.speak(speech).ask(
+            reprompt).set_card(SimpleCard(
+                data[prompts.SKILL_NAME], speech))
+        return handler_input.response_builder.response
+
+class CancelOrStopIntentHandler(AbstractRequestHandler):
+    """Single handler for Cancel and Stop Intent."""
+
+    def can_handle(self, handler_input):
+        # type: (HandlerInput) -> bool
+        return (is_intent_name("AMAZON.CancelIntent")(handler_input) or
+                is_intent_name("AMAZON.StopIntent")(handler_input))
+
+    def handle(self, handler_input):
+        # type: (HandlerInput) -> Response
+        logger.info("In CancelOrStopIntentHandler")
+
+        # get localization data
+        data = handler_input.attributes_manager.request_attributes["_"]
+
+        speech = data[prompts.STOP_MESSAGE]
+        handler_input.response_builder.speak(speech)
+        return handler_input.response_builder.response
+
+class FallbackIntentHandler(AbstractRequestHandler):
+    """Handler for Fallback Intent.
+    AMAZON.FallbackIntent is only available in en-US locale.
+    This handler will not be triggered except in that locale,
+    so it is safe to deploy on any locale.
+    """
+
+    def can_handle(self, handler_input):
+        # type: (HandlerInput) -> bool
+        return is_intent_name("AMAZON.FallbackIntent")(handler_input)
+
+    def handle(self, handler_input):
+        # type: (HandlerInput) -> Response
+        logger.info("In FallbackIntentHandler")
+
+        # get localization data
+        data = handler_input.attributes_manager.request_attributes["_"]
+
+        speech = data[prompts.FALLBACK_MESSAGE]
+        reprompt = data[prompts.FALLBACK_REPROMPT]
+        handler_input.response_builder.speak(speech).ask(
+            reprompt)
+        return handler_input.response_builder.response
+
+
+class SessionEndedRequestHandler(AbstractRequestHandler):
+    """Handler for Session End."""
+
+    def can_handle(self, handler_input):
+        # type: (HandlerInput) -> bool
+        return ask_utils.is_request_type("SessionEndedRequest")(handler_input)
+
+    def handle(self, handler_input):
+        # type: (HandlerInput) -> Response
+
+        # Any cleanup logic goes here.
+
+        return handler_input.response_builder.response
 
 class CatchAllExceptionHandler(AbstractExceptionHandler):
     """Generic error handling to capture any syntax or routing errors. If you receive an error
@@ -120,6 +207,10 @@ sb = SkillBuilder()
 # register request / intent handlers
 sb.add_request_handler(GetDataApiHandler())
 sb.add_request_handler(SessionEndedRequestHandler())
+sb.add_request_handler(HelpIntentHandler())
+sb.add_request_handler(GetServerNameHandler())
+sb.add_request_handler(CancelOrStopIntentHandler())
+sb.add_request_handler(FallbackIntentHandler())
 sb.add_request_handler(IntentReflectorHandler())
 
 # register exception handlers
